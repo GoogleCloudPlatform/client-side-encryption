581fe926677d7b80b58e9bdbc2b65e39c12a5a5e
Jason Callaway <jasoncallaway@google.com>
2020-11-23 11:35:03 -0500

    changing test layout to support both wrapper and deployment testing

--- test_wrapper.py
+++ test_wrapper.py
@@ -19,5 +19,5 @@ import unittest
 
 if __name__ == '__main__':
   unittest.TestLoader.sortTestMethodsUsing = None
-  suite = unittest.TestLoader().discover('tests/', pattern='*_test.py')
+  suite = unittest.TestLoader().discover('tests/wrapper', pattern='*_test.py')
   unittest.TextTestRunner(verbosity=4).run(suite)
