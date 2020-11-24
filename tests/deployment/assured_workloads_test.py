cfba8550027c3e8c6d76bd6ba7c74b0c2d8370ba
Jason Callaway <jasoncallaway@google.com>
2020-11-24 18:08:45 -0500

    working on aw unit test

--- tests/deployment/assured_workloads_test.py
+++ tests/deployment/assured_workloads_test.py
@@ -23,7 +23,7 @@ from oauth2client.client import GoogleCredentials
 # Set dunder unittest in the global scope for pretty unit test results
 __unittest = True
 
-class TestEnabledAPIs(unittest.TestCase):
+class TestAssuredWorkloads(unittest.TestCase):
   """Test cases for Assured Workloads. Note that this test requires the
   PROJECT_ID environment variable to be set."""
 
@@ -31,49 +31,29 @@ class TestEnabledAPIs(unittest.TestCase):
     """Call super class' setup and define some variables."""
     super().setUp()
     PROJECT_ID = os.getenv('PROJECT_ID', 'my-project')
+    ORG_ID = os.getenv('ORG_ID', 'my-org')
+    REGION = os.getenv('REGION', 'us-central1')
     self.project_id = PROJECT_ID
+    self.org_id = ORG_ID
+    self.region = REGION
 
   def test_assured_workloads(self):
+    """Testing Assured Workload settings."""
     # Get application-default credentials and initialize API client
     credentials = GoogleCredentials.get_application_default()
     project = 'projects/' + self.project_id
     service = discovery.build('assuredworkloads',
                               'v1beta1',
                               credentials=credentials)
-    pause = True
-
-  # def test_enabled_apis(self):
-  #   """Testing to see if any APIs need to be flipped."""
-  #   # Get application-default credentials and initialize API client
-  #   credentials = GoogleCredentials.get_application_default()
-  #   project = 'projects/' + self.project_id
-  #   service = discovery.build('serviceusage', 'v1', credentials=credentials)
-  #   try:
-  #     apis = {'ENABLED': [], 'DISABLED': []}
-  #     # Set the next page token to 'first' just so we know not to include that
-  #     # argument in the first services().list() call
-  #     next_page = 'first'
-  #     # Iterate over pages of API responses
-  #     while next_page:
-  #       if next_page == 'first':
-  #         request = service.services().list(parent=project)
-  #       else:
-  #         request = service.services().list(parent=project, pageToken=next_page)
-  #       response = request.execute()
-  #       if 'nextPageToken' in response:
-  #         next_page = response['nextPageToken']
-  #       else:
-  #         next_page = False
-  #       # Now read the enabled and disabled APIs and put them in the appropriate
-  #       # list
-  #       services = response.get('services')
-  #       for index in range(len(services)):
-  #         item = services[index]
-  #         if item.get('config').get('name'):
-  #           apis[item['state']].append(item['config']['name'])
-  #       # Assert that the expected enabled APIs are equal to the actual enabled
-  #       # APIs
-  #     self.assertSetEqual(self.enabled_apis, set(apis['ENABLED']),
-  #                         'APIs that need to be enabled or disabled')
-  #   except Exception as e:
-  #     raise e
+    try:
+      request = service.organizations().locations().workloads().list(
+          parent='organizations/{org}/locations/{loc}'.format(
+              org=self.org_id,
+              loc=self.region
+          )
+      )
+      workloads = request.execute()
+      w = workloads['workloads']
+      # left off here. err 403 on api endpoint
+    except Exception as e:
+      pass
