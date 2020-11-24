63177e8d4de6769e6aadfbfe73294a0e30b2d0ae
Jason Callaway <jasoncallaway@google.com>
2020-11-24 16:51:08 -0500

    removed unnecessary author lines from license headers

--- test_deployment.py
+++ test_deployment.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # Copyright 2020 Google LLC
-# Author: jasoncallaway@google.com (Jason Callaway)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
